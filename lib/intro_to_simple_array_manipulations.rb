bc6c3f96cbb449916b7d03e007d49eac4f1c67ae
Yannick <ydjoumbi@gmail.com>
2020-04-12 00:55:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,5 +7,23 @@ def using_unshift (array, string)
 end
 
 def using_pop (array)
-  array.pop
+  array.using
+end
+
+def pop_with_args (array)
+  array.pop(2)
+end
+
+def using_shift (array)
+  array.shift 
+end
+
+def shift_with_args (array)
+  array.shift(2)
+end
+
+
+
+
+
   
